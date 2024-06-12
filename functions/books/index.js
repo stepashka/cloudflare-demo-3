16b09f549a2bc055edabbd3d7dd099eff51b2a81
Rishi Raj Jain <jain71000@gmail.com>
2024-06-13 00:34:32 +0530

    Update index.js

--- functions/books/index.js
+++ functions/books/index.js
@@ -1,7 +1,6 @@
 import { Client } from '@neondatabase/serverless';
 
 export async function onRequestGet(context) {
-  return new Response(context.env.DATABASE_URL ? JSON.stringify(context.env.DATABASE_URL) : "no val")
   const client = new Client(context.env.DATABASE_URL);
   await client.connect();
   // Logic to fetch books from your database
